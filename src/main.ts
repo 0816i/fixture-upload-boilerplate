274c5c95759adc3495be303cda731ff003ac3c40
Roux <0816i@naver.com>
2022-11-23 17:32:34 +0900

    feat: service 뼈대 생성

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import dotenv from "dotenv";
+import { Service } from "src/service/Service";
 
 dotenv.config();
+
+async function main() {
+  const myService = new Service();
+  await myService.start();
+}
+
+main();
